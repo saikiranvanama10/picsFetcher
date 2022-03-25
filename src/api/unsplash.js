f87a61db0be92615c9033c5e609485a3394588b8
saikiranvanama10 <85451191+saikiranvanama10@users.noreply.github.com>
2022-03-24 23:19:57 -0700

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID vAoarQTM0HBZsCQ9Jb_zohNX2He8sYrtu129TVe5MJg'
+        Authorization: 'Client-ID <Add unsplash API key>'
     }
 });
